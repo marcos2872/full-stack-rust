566c4a8cb11590cc76e6ea68e8048c160cf17fc6
Marcos Brito <marcos.brito@sp.senai.br>
2025-09-23 10:47:59 -0300

    Replace Box<Error> with custom DataError type

--- src/data/user.rs
+++ src/data/user.rs
@@ -1,18 +1,12 @@
+use crate::data::errors::DataError;
 use sqlx::PgPool;
 
 pub async fn create_user(
     pool: &PgPool,
     email: &str,
     password: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), DataError> {
     let hashed_password = bcrypt::hash(password, bcrypt::DEFAULT_COST)?;
-
-    // let query = r#"
-    // INSERT INTO users(email, password)
-    // VALUES($1, $2)
-    // "#;
-    //
-    // let result = sqlx::query(query).bind(email).bind(hashed_password).execute(pool).await;
 
     sqlx::query!(
         "INSERT INTO users(email, password) VALUES($1, $2)",
@@ -21,5 +15,6 @@ pub async fn create_user(
     )
     .execute(pool)
     .await?;
+
     Ok(())
 }
