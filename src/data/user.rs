69118a7d18a51ea9cadf293ee261b6ad853c45ae
Marcos Brito <marcos.brito@sp.senai.br>
2025-09-23 15:53:52 -0300

    Handle duplicate email errors in user creation

--- src/data/user.rs
+++ src/data/user.rs
@@ -1,11 +1,7 @@
 use crate::data::errors::DataError;
 use sqlx::PgPool;
 
-pub async fn create_user(
-    pool: &PgPool,
-    email: &str,
-    password: &str,
-) -> Result<(), DataError> {
+pub async fn create_user(pool: &PgPool, email: &str, password: &str) -> Result<(), DataError> {
     let hashed_password = bcrypt::hash(password, bcrypt::DEFAULT_COST)?;
 
     sqlx::query!(
@@ -14,7 +10,17 @@ pub async fn create_user(
         hashed_password
     )
     .execute(pool)
-    .await?;
+    .await
+    .map_err(|err| match err {
+        sqlx::Error::Database(e) => {
+            if e.constraint() == Some("users_email_key") {
+                DataError::FailedQuery("This email address is already used".to_string())
+            } else {
+                DataError::Internal(e.to_string())
+            }
+        }
+        e => DataError::Query(e),
+    })?;
 
     Ok(())
 }
